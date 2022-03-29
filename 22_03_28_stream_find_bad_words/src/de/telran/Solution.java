87522628e5d57c19e18ac9d188a5f2bc508715e0
olever <olever73@gmail.com>
2022-03-29 23:19:53 +0200

    add "home work"

--- 22_03_28_stream_find_bad_words/src/de/telran/Solution.java
+++ 22_03_28_stream_find_bad_words/src/de/telran/Solution.java
@@ -13,15 +13,16 @@ public class Solution {
                 "set", "terse", "tree", "sea", "sip", "snow", "snail", "sly",
                 "six", "sod", "see", "sit", "sad", "wry", "why");
 
-        Predicate<String> p1 = e -> e.startsWith("s") && e.startsWith("w");
+        Predicate<String> p1 = e -> e.startsWith("s") || e.startsWith("w");
         Predicate<String> p2 = e -> e.endsWith("y");
         Predicate<String> p3 = e -> e.length() == 3;
 
         var prs = List.of(p1, p2, p3);
 
         var result = words.stream()
-                .filter(prs.stream().reduce(x -> true, Predicate::and))  // пересечение and(объединение or)
+                .filter(prs.stream().reduce(x -> true, Predicate::and))
                 .collect(Collectors.toList());
+
         result.forEach(System.out::println);
     }
 }
